909bc818bd57de9da69c1837dc9058c5b6c46158
rluckom <raphaelluckom@gmail.com>
2013-11-21 18:10:13 -0500

    documentated WebTestSection and WebTester

--- src/WebTestSection.py
+++ src/WebTestSection.py
@@ -16,7 +16,17 @@ from src.platypus_utils import std_to_px, std_to_points
 
 class WebTestSection(object):
 
+    """Builds a list of platypus flowables representinng a report section."""
+
     def __init__(self, heading_head, heading_text, header_style, text_style):
+        """constructor.
+
+        @param heading_head (str) : chapter title.
+        @param header_text (str) : displayed after the title.
+        @param header_style (ParagraphStyle) : style to use for header
+        @param text_style (ParagraphStyle) : style to use for text
+
+        """
         self.max_x = 200
         self.max_y = 684
         self.spacer = 20
@@ -29,6 +39,15 @@ class WebTestSection(object):
                               + (text_style.leading * len(self.header_text)))
 
     def _resize_img_to_scale(self, img):
+        """scale down an image until it fits within self.max_x.
+
+        Converts self.max_x and img width to inches (img width @300dpi),
+        then downsamples the img if necessary so it fits width @300dpi
+
+        @param img (PIL.Image) : image to resize
+        @return PIL.Image : resized image
+
+        """
         width, height = img.size
         scale = 1
         if px_to_std(width) > points_to_std(self.max_x):
@@ -36,8 +55,13 @@ class WebTestSection(object):
         return img.resize((int(scale * width), int(scale * height)),
                           IM.ANTIALIAS)
 
-    def chunk_img(self, fn):
-        img = self._resize_img_to_scale(IM.open(fn))
+    def chunk_img(self, filename):
+        """Flow image across pages, append chunks to self.imgs.
+
+        @filename (str) : full path to img.
+
+        """
+        img = self._resize_img_to_scale(IM.open(filename))
         width, height = img.size
         pts_width = int(math.floor(std_to_points(px_to_std(width))))
         vertical_inches = px_to_std(height)
@@ -55,19 +79,28 @@ class WebTestSection(object):
             top = bottom
             bottom += int(math.floor(std_to_px(chunk_height)))
             bbox = [left, top, right, bottom]
-            chunk = img.crop(bbox)
-            chunk_filename = tempfile.mkstemp(suffix='.png')[1]
-            chunk.save(chunk_filename, dpi=(200, 200), quality=95)
-            self.imgs.append(Image(chunk_filename,
-                                   width=pts_width,
-                                   height=pts_height))
+            self._add_chunk(img.crop(bbox), pts_width, pts_height)
+
+    def _add_chunk(self, chunk, pts_width, pts_height):
+        """Add an image chunk to self's list of flowables."""
+        chunk_filename = tempfile.mkstemp(suffix='.png')[1]
+        chunk.save(chunk_filename, dpi=(200, 200), quality=95)
+        self.imgs.append(Image(chunk_filename,
+                               width=pts_width,
+                               height=pts_height))
 
     def next_page_height(self):
+        """Make a generator that returns the next page height.
+
+        @return generator : __next__ method returns the next page height.
+
+        """
         yield math.floor(points_to_std(self.max_y - self.header_offset))
         while True:
             yield points_to_std(self.max_y)
 
     def list_elements(self):
+        """@return list : list of the self's flowables."""
         head = Paragraph(self.heading_head, self.header_style)
         text = [Paragraph(x, self.text_style) for x in self.header_text]
         return [head] + text + [Spacer(self.max_x, self.spacer)] + self.imgs
--- src/WebTester.py
+++ src/WebTester.py
@@ -1,5 +1,5 @@
 """
-Creates a report showing listed resources under listed driver capabilities
+Creates a report showing listed resources under listed driver capabilities.
 
 @author raphaelluckom@gmail.com
 @license GPL v2.0 or later
@@ -20,17 +20,31 @@ WebTest = namedtuple('WebTest', ['browser_name',
 
 class WebTester(object):
 
+    """Visits pages in browsers, takes screenshots."""
+
     def __init__(self, tests, report_name):
+        """constructor.
+
+        @param tests (list) : WebTest namedtuples
+        @param report_name (str) : name for the report. Used in filename.
+
+        """
         self._tests = tests
         self._results = WebTestResults()
         self._report_name = report_name
 
     def start(self):
+        """run the tests."""
         for test in self._tests:
             self.run_test(test)
         self._results.write_to_file(self._report_name)
 
     def run_test(self, test):
+        """run test.
+
+        @param test (WebTest) : webtest namedtuple
+
+        """
         driver = webdriver.Remote(test.browser_node_uri,
                                   test.browser_selenium_spec)
         for resource in test.resources_to_test:
